1b83a5cc95b6569884c40cb2f98755e06393cf0a
p22dhealy <p22dhealy@fairfieldprep.org>
2021-11-02 10:21:25 -0400

    submit half of hw 5-3 up to question 3

---
+++ hw5-3.py
@@ -0,0 +1,11 @@
+#author: DMH 11/2/21
+
+import calendar
+#Question 1
+calendar.TextCalendar().pryear(2020)
+
+#Question 2
+calendar.TextCalendar(calendar.SUNDAY).pryear(2020)
+
+#Question 3
+calendar.TextCalendar().pryear(2004, 4)
